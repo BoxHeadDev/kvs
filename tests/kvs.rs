1cc04d7b476ae1f00c3ff51012fc34e68af9241c
chris wren <chriswrendeveloper@gmail.com>
2025-06-30 20:44:03 +0100

    solution: compaction test

--- tests/kvs.rs
+++ tests/kvs.rs
@@ -1,5 +1,6 @@
 use kvs::{KvStore, Result};
 use tempfile::TempDir;
+use walkdir::WalkDir;
 
 //
 // Test 1: Should get previously stored value
@@ -88,4 +89,50 @@ fn remove_key() -> Result<()> {
     assert!(store.remove("key1".to_owned()).is_ok());
     assert_eq!(store.get("key1".to_owned())?, None);
     Ok(())
+}
+
+// Insert data until total size of the directory decreases.
+// Test data correctness after compaction.
+#[test]
+fn compaction() -> Result<()> {
+    let temp_dir = TempDir::new().expect("unable to create temporary working directory");
+    let mut store = KvStore::open(temp_dir.path())?;
+
+    let dir_size = || {
+        let entries = WalkDir::new(temp_dir.path()).into_iter();
+        let len: walkdir::Result<u64> = entries
+            .map(|res| {
+                res.and_then(|entry| entry.metadata())
+                    .map(|metadata| metadata.len())
+            })
+            .sum();
+        len.expect("fail to get directory size")
+    };
+
+    let mut current_size = dir_size();
+    for iter in 0..1000 {
+        for key_id in 0..1000 {
+            let key = format!("key{}", key_id);
+            let value = format!("{}", iter);
+            store.set(key, value)?;
+        }
+
+        let new_size = dir_size();
+        if new_size > current_size {
+            current_size = new_size;
+            continue;
+        }
+        // Compaction triggered.
+
+        drop(store);
+        // reopen and check content.
+        let mut store = KvStore::open(temp_dir.path())?;
+        for key_id in 0..1000 {
+            let key = format!("key{}", key_id);
+            assert_eq!(store.get(key)?, Some(format!("{}", iter)));
+        }
+        return Ok(());
+    }
+
+    panic!("No compaction detected");
 }

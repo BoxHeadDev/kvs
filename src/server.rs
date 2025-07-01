93d57f830ebc455728d44ff12eabe73034f6f975
chris wren <chriswrendeveloper@gmail.com>
2025-07-01 12:39:33 +0100

    solution: server rpc

--- src/server.rs
+++ src/server.rs
@@ -1,8 +1,13 @@
-use crate::Result;
-use log::error;
-use std::net::{TcpListener, ToSocketAddrs};
+use crate::common::{GetResponse, RemoveResponse, Request, SetResponse};
+use crate::{KvStore, Result};
+use log::{debug, error};
+use serde_json::Deserializer;
+use std::io::{BufReader, BufWriter, Write};
+use std::net::{TcpListener, TcpStream, ToSocketAddrs};
 
-pub struct KvsServer {}
+pub struct KvsServer {
+    engine: KvStore,
+}
 
 impl KvsServer {
     pub fn run<A: ToSocketAddrs>(self, addr: A) -> Result<()> {
@@ -15,7 +20,45 @@ impl KvsServer {
                     continue;
                 }
             };
+
+            if let Err(e) = self.serve(stream) {
+                error!("Error serving client: {}", e);
+            }
         }
+        Ok(())
+    }
+
+    fn serve(&self, tcp: TcpStream) -> Result<()> {
+        let peer_addr = tcp.peer_addr()?;
+        let reader = BufReader::new(tcp.try_clone()?);
+        let mut writer = BufWriter::new(tcp);
+        let reqs = Deserializer::from_reader(reader).into_iter::<Request>();
+
+        for req in reqs {
+            let req = req?;
+            debug!("Request from {}: {:?}", peer_addr, req);
+            let resp = match req {
+                Request::Get { key } => self
+                    .engine
+                    .get(key)
+                    .map(GetResponse::Ok)
+                    .unwrap_or_else(|e| GetResponse::Err(e.to_string())),
+                Request::Set { key, value } => self
+                    .engine
+                    .set(key, value)
+                    .map(|_| SetResponse::Ok(()))
+                    .unwrap_or_else(|e| SetResponse::Err(e.to_string())),
+                Request::Remove { key } => self
+                    .engine
+                    .remove(key)
+                    .map(|_| RemoveResponse::Ok(()))
+                    .unwrap_or_else(|e| RemoveResponse::Err(e.to_string())),
+            };
+            serde_json::to_writer(&mut writer, &resp)?;
+            writer.flush()?;
+            debug!("Response sent to {}: {:?}", peer_addr, resp);
+        }
+
         Ok(())
     }
 }

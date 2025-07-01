92388909b18465fa42faa1b71d055bddfce526ee
chris wren <chriswrendeveloper@gmail.com>
2025-07-01 12:31:56 +0100

    solution: client rpc

--- src/client.rs
+++ src/client.rs
@@ -1,10 +1,13 @@
 use crate::Result;
-use std::io::{BufReader, BufWriter};
+use crate::common::{GetResponse, RemoveResponse, Request, SetResponse};
+use serde::Deserialize;
+use serde_json::de::{Deserializer, IoRead};
+use std::io::{BufReader, BufWriter, Write};
 use std::net::{TcpStream, ToSocketAddrs};
 
 /// Key value store client
 pub struct KvsClient {
-    reader: BufReader<TcpStream>,
+    reader: Deserializer<IoRead<BufReader<TcpStream>>>,
     writer: BufWriter<TcpStream>,
 }
 
@@ -14,8 +17,40 @@ impl KvsClient {
         let tcp_reader = TcpStream::connect(addr)?;
         let tcp_writer = tcp_reader.try_clone()?;
         Ok(KvsClient {
-            reader: BufReader::new(tcp_reader),
+            reader: Deserializer::from_reader(BufReader::new(tcp_reader)),
             writer: BufWriter::new(tcp_writer),
         })
+    }
+    /// Get the value of a given key from the server.
+    pub fn get(&mut self, key: String) -> Result<Option<String>> {
+        serde_json::to_writer(&mut self.writer, &Request::Get { key })?;
+        self.writer.flush()?;
+        let resp = GetResponse::deserialize(&mut self.reader)?;
+        match resp {
+            GetResponse::Ok(value) => Ok(value),
+            GetResponse::Err(msg) => Err(msg),
+        }
+    }
+
+    /// Set the value of a string key in the server.
+    pub fn set(&mut self, key: String, value: String) -> Result<()> {
+        serde_json::to_writer(&mut self.writer, &Request::Set { key, value })?;
+        self.writer.flush()?;
+        let resp = SetResponse::deserialize(&mut self.reader)?;
+        match resp {
+            SetResponse::Ok(_) => Ok(()),
+            SetResponse::Err(msg) => Err(msg),
+        }
+    }
+
+    /// Remove a string key in the server.
+    pub fn remove(&mut self, key: String) -> Result<()> {
+        serde_json::to_writer(&mut self.writer, &Request::Remove { key })?;
+        self.writer.flush()?;
+        let resp = RemoveResponse::deserialize(&mut self.reader)?;
+        match resp {
+            RemoveResponse::Ok(_) => Ok(()),
+            RemoveResponse::Err(msg) => Err(msg),
+        }
     }
 }

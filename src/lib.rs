a9a27a5a9f4cc9ec063efac75b1e8cec45098c9e
chris wren <chriswrendeveloper@gmail.com>
2025-07-02 15:04:20 +0100

    solution: naive thread pool

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ pub use client::KvsClient;
 pub use engines::{KvStore, KvsEngine, SledKvsEngine};
 pub use error::{KvsError, Result};
 pub use server::KvsServer;
+pub mod thread_pool;
 
 mod client;
 mod common;
